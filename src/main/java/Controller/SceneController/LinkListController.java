eae363001e8553682e3d78836c54315fef7488a6
sooyoung <hasu0907*>
2021-09-25 16:05:38 -0400

    initial commite

--- src/main/java/Controller/SceneController/LinkListController.java
+++ src/main/java/Controller/SceneController/LinkListController.java
@@ -3,6 +3,8 @@ package Controller.SceneController;
 import Model.Model;
 import Object.Link;
 import Data.LinkData;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
@@ -10,6 +12,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 import static Controller.SceneController.ScreenController.switchScreen;
 
@@ -21,6 +31,8 @@ public class LinkListController {
     private Button launchBtn;
     @FXML
     private Button editBtn;
+
+    private ObservableList<Integer> selectedLinkTitleIndex;
 
 
     public void initialize() {
@@ -37,7 +49,12 @@ public class LinkListController {
         launchBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-
+                getLinkToLaunch();
+                Link selectedLink = linkData.getLinkList().get(selectedLinkTitleIndex.get(0));
+                int linkIndex = Model.getDb().getLinkList().indexOf(selectedLink);
+                Link link = Model.getDb().getLinkList().get(linkIndex);
+                //                    openWebpage(link.getLink());
+                openUrlInBrowser(link.getLink());
             }
         });
 
@@ -47,6 +64,28 @@ public class LinkListController {
                 switchScreen("View/ListEdit.fxml");
             }
         });
+    }
+
+    private void getLinkToLaunch() {
+        selectedLinkTitleIndex = linkList.getSelectionModel().getSelectedIndices();
+    }
+
+    private void openUrlInBrowser(String url) {
+        Runtime runtime = Runtime.getRuntime();
+        String[] args = { "osascript", "-e", "open location \"" + url + "\"" };
+        try {
+            Process process = runtime.exec(args);
+        }
+        catch (IOException e) {
+            // do what you want with this
+        }
+    }
+
+    public void openWebpage(String urlString) throws IOException {
+        URL myURL = new URL(urlString);
+        URLConnection myURLConnection = myURL.openConnection();
+        myURLConnection.connect();
+        System.out.println(myURLConnection);
     }
 
 
