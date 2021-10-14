87a5afff277a201785449446b2482f72a6389d77
Sooyoung <jeonso3412@gmail.com>
2021-10-14 10:47:01 -0400

    jar deployment test

--- src/main/java/Controller/SceneController/LinkListController.java
+++ src/main/java/Controller/SceneController/LinkListController.java
@@ -3,8 +3,6 @@ package Controller.SceneController;
 import Model.Model;
 import Object.Link;
 import Data.LinkData;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
@@ -13,20 +11,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 
-import java.awt.*;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
 
 import static Controller.SceneController.ScreenController.switchScreen;
 
 public class LinkListController {
 
     @FXML
-    private ListView linkList;
+    private ListView<String> linkList;
     @FXML
     private Button launchBtn;
     @FXML
@@ -34,30 +27,44 @@ public class LinkListController {
 
     private ObservableList<Integer> selectedLinkTitleIndex;
 
-
+    /**
+     * Initialize Link List Controller
+     */
     public void initialize() {
 
+        // Get initialized link data
         LinkData linkData = Model.getDb();
         ObservableList<String> obsLinkList = FXCollections.observableArrayList();
 
+        // Add each link data in the observable list
         for (Link link : linkData.getLinkList()) {
             obsLinkList.add(link.getTitle());
         }
 
+        // Set observable list with list display
         linkList.setItems(obsLinkList);
 
+        // Link List Launch button handler
         launchBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
+                // Get and set selected link's index
                 getLinkToLaunch();
                 Link selectedLink = linkData.getLinkList().get(selectedLinkTitleIndex.get(0));
                 int linkIndex = Model.getDb().getLinkList().indexOf(selectedLink);
                 Link link = Model.getDb().getLinkList().get(linkIndex);
-                //                    openWebpage(link.getLink());
+
+                // Run URL based on the right OS (Window, Mac)
+                try {
+                    openWebpage(link.getLink());
+                } catch (IOException | URISyntaxException e) {
+                    e.printStackTrace();
+                }
                 openUrlInBrowser(link.getLink());
             }
         });
 
+        // Link List Edit button handler
         editBtn.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
@@ -66,10 +73,17 @@ public class LinkListController {
         });
     }
 
+    /**
+     * Get and set selected link's index
+     */
     private void getLinkToLaunch() {
         selectedLinkTitleIndex = linkList.getSelectionModel().getSelectedIndices();
     }
 
+    /**
+     * URL launcher for Window
+     * @param url
+     */
     private void openUrlInBrowser(String url) {
         Runtime runtime = Runtime.getRuntime();
         String[] args = { "osascript", "-e", "open location \"" + url + "\"" };
@@ -81,11 +95,14 @@ public class LinkListController {
         }
     }
 
-    public void openWebpage(String urlString) throws IOException {
-        URL myURL = new URL(urlString);
-        URLConnection myURLConnection = myURL.openConnection();
-        myURLConnection.connect();
-        System.out.println(myURLConnection);
+    /**
+     * URL launcher for Mac
+     * @param urlString
+     * @throws IOException
+     * @throws URISyntaxException
+     */
+    public void openWebpage(String urlString) throws IOException, URISyntaxException {
+        Runtime.getRuntime().exec(new String[]{"cmd", "/c","start chrome " + urlString});
     }
 
 
